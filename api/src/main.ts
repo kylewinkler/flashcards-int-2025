ea7c46424c1fb107b1ad0699d5a72ff49a41a4c6
Kyle <kwinkler@caxy.com>
2025-07-31 16:15:07 -0500

    create .env and setup config

--- api/src/main.ts
+++ api/src/main.ts
@@ -4,6 +4,7 @@ import { AppModule } from './app.module';
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   app.enableCors();
+  console.log('\n\n', process.env.MONGO_CONNECTION_STRING , '\n\n')
   await app.listen(process.env.PORT ?? 3001);
 }
 bootstrap();
