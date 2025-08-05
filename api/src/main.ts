2d1897a2a247094b6fa09086ac8fa5c803e9b902
Kyle <kwinkler@caxy.com>
2025-08-05 12:28:43 -0500

    setup mongodb connect

--- api/src/main.ts
+++ api/src/main.ts
@@ -4,7 +4,8 @@ import { AppModule } from './app.module';
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   app.enableCors();
-  console.log('\n\n', process.env.MONGO_CONNECTION_STRING , '\n\n')
   await app.listen(process.env.PORT ?? 3001);
+  console.log('server listening on port: ', process.env.PORT)
 }
+
 bootstrap();
